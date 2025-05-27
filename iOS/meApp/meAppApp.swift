94d3f2574ce972cc692a5962ee6a2c919b9aafb6
bchittibabu <95400902+bchittibabu@users.noreply.github.com>
2025-05-27 12:04:46 +0530

    MA-17 Initial project setup

---
+++ iOS/meApp/meAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  meAppApp.swift
+//  meApp
+//
+//  Created by Barath Chittibabu on 27/05/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct meApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
