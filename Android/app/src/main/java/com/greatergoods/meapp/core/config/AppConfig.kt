ada7d5ec26c529efd323217eefd56a5079a53f24
rengadevi <vrengadevi@greatergoods.com>
2025-05-28 16:45:48 +0530

    Clean up comments in AppConfig

--- Android/app/src/main/java/com/greatergoods/meapp/core/config/AppConfig.kt
+++ Android/app/src/main/java/com/greatergoods/meapp/core/config/AppConfig.kt
@@ -1,6 +1,6 @@
 package com.greatergoods.meapp.core.config
 
 object AppConfig {
-    const val BASE_URL = "https://api.weightgurus.com/v3" // Replace with your actual API base URL,
+    const val BASE_URL = "https://api.weightgurus.com/v3"
     const val BASE_URL_HEADER = "Base-url"
 }
