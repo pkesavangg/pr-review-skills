0215c61b7ca93061e0570ad6bd4ab43d7946ad3a
gpoongkuzhali <gpoongkuzhali@greatergoods.com>
2025-05-30 16:04:20 +0530

    MA-119 - Refactored file structure

--- Android/app/src/main/java/com/greatergoods/meapp/core/di/DatabaseModule.kt
+++ Android/app/src/main/java/com/greatergoods/meapp/core/di/DatabaseModule.kt
@@ -1,5 +1,7 @@
-package com.greatergoods.meapp.data.storage.db.di
+package com.greatergoods.meapp.core.di
 
+import android.content.Context
+import androidx.room.Room
 import com.greatergoods.meapp.data.storage.db.AppDatabase
 import com.greatergoods.meapp.data.storage.db.dao.AccountDao
 import com.greatergoods.meapp.data.storage.db.dao.DeviceDao
@@ -7,12 +9,13 @@ import com.greatergoods.meapp.data.storage.db.dao.EntryDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object DaosModule {
+object DatabaseModule {
 
     @Provides
     @Singleton
