f16d98c8db9f0c404b4e708b477494adec70a3af
Selva <kselvakumar@dmdbrands.com>
2025-06-16 12:16:45 +0530

    MA-334 Initialize base reducer configuration and implement app state management

--- Android/app/src/main/java/com/greatergoods/meapp/features/common/viewmodel/BaseViewModel.kt
+++ Android/app/src/main/java/com/greatergoods/meapp/features/common/viewmodel/BaseViewModel.kt
@@ -2,31 +2,16 @@ package com.greatergoods.meapp.features.common.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.greatergoods.meapp.core.service.IAppEventService
-import com.greatergoods.meapp.core.shared.utilities.logging.AppLog
 import com.greatergoods.meapp.features.common.service.DialogQueueService
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
-abstract class BaseViewModel : ViewModel() {
+@HiltViewModel
+ class BaseViewModel @Inject constructor() : ViewModel() {
     @Inject
     lateinit var navigationService: IAppEventService
 
     @Inject
     lateinit var dialogQueueService: DialogQueueService
 
-    init {
-        injectDependencies()
-    }
-
-    private fun injectDependencies() {
-        AppLog.e("HomeViewModel", "HomeViewModel init")
-
-        // val entryPoint =
-        //     EntryPointAccessors.fromApplication(
-        //         MeAppApplication.instance,
-        //         ViewModelServiceEntryPoint::class.java,
-        //     )
-        //
-        // navigationService = entryPoint.navigationService
-        // dialogQueueService = entryPoint.dialogQueueService
-    }
 }
