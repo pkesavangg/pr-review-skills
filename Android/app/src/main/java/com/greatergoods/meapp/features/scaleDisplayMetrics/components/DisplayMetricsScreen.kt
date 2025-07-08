bdfde8ef4de23aed7a074a2b1b23a778b0abcdd5
gpoongkuzhali <gpoongkuzhali@greatergoods.com>
2025-07-08 10:22:04 +0530

    MA-616 - Created scale display metrics folder structure.

---
+++ Android/app/src/main/java/com/greatergoods/meapp/features/scaleDisplayMetrics/components/DisplayMetricsScreen.kt
@@ -0,0 +1,17 @@
+package com.greatergoods.meapp.features.scaleDisplayMetrics.components
+
+import androidx.compose.runtime.Composable
+import com.greatergoods.meapp.features.common.components.PreviewTheme
+import com.greatergoods.meapp.theme.MeAppTheme
+
+@Composable
+fun DisplayMetricsScreen() {
+}
+
+@PreviewTheme
+@Composable
+fun DisplayMetricsScreenPreview() {
+  MeAppTheme {
+    DisplayMetricsScreen()
+  }
+}
