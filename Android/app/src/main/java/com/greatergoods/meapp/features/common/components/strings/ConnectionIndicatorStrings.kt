c36cda56ee97d104c8ec446c4d28f9161b86d1ca
gpoongkuzhali <gpoongkuzhali@greatergoods.com>
2025-07-10 20:00:08 +0530

    MA-644 - Implemented Loader and connection Indicator for scale setup

---
+++ Android/app/src/main/java/com/greatergoods/meapp/features/common/components/strings/ConnectionIndicatorStrings.kt
@@ -0,0 +1,12 @@
+package com.greatergoods.meapp.features.common.components.strings
+
+/**
+ * String constants for the ConnectionIndicator component.
+ */
+object ConnectionIndicatorStrings {
+    /** Content description for connecting state */
+    const val ConnectingDescription = "Connection in progress"
+    
+    /** Content description for failed state */
+    const val FailedDescription = "Connection failed"
+} 
