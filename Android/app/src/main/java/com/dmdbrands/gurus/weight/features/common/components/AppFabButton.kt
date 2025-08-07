62a2a1c9b7449f279f348fc132f14f0546ac8b5d
rengadevi <vrengadevi@greatergoods.com>
2025-08-07 20:10:24 +0530

    Refactor AppFabButton and WeightOnlyModePopup
    - Updated `AppFabButton.kt`:
        - Changed FAB container color to be semi-transparent by default and opaque when pressed or dragged.
        - Added elevation to the FAB when pressed or dragged.
        - Set icon tint to `MeTheme.colorScheme.inverseAction`.
        - Added `isDraggingOn` state to track drag gestures.
        - Updated `detectDragGestures` to include `onDragStart` and `onDragCancel` to manage `isDraggingOn` state.
        - Used `MutableInteractionSource` to update `isPressed` state based on press interactions.
    - Updated `WeightOnlyModePopup.kt`:
        - Increased padding for the main column.
        - Increased size of the `AppIcon`.

--- Android/app/src/main/java/com/dmdbrands/gurus/weight/features/common/components/AppFabButton.kt
+++ Android/app/src/main/java/com/dmdbrands/gurus/weight/features/common/components/AppFabButton.kt
@@ -4,6 +4,8 @@ import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.VectorConverter
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.ime
@@ -12,12 +14,17 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
@@ -35,101 +42,128 @@ import kotlinx.coroutines.launch
 
 @Composable
 fun AppFab(
-  offsetX: Dp = 0.dp,
-  offsetY: Dp = 0.dp,
-  showWeightOnlyModeAlert: Boolean = false,
-  onClick: () -> Unit
+    offsetX: Dp = 0.dp,
+    offsetY: Dp = 0.dp,
+    showWeightOnlyModeAlert: Boolean = false,
+    onClick: () -> Unit
 ) {
-  val scope = rememberCoroutineScope()
-  // Use a single Animatable for the Offset to ensure path is always a straight line.
-  val position = remember { Animatable(Offset(offsetX.value, offsetY.value), Offset.VectorConverter) }
+    val scope = rememberCoroutineScope()
+    var isPressed by remember { mutableStateOf(false) }
+    var isDraggingOn by remember { mutableStateOf(false) }
 
-  val density = LocalDensity.current
-  val configuration = LocalConfiguration.current
+    // Use a single Animatable for the Offset to ensure path is always a straight line.
+    val position =
+        remember { Animatable(Offset(offsetX.value, offsetY.value), Offset.VectorConverter) }
 
-  // Get screen dimensions
-  val screenWidth = configuration.screenWidthDp.dp
-  val screenHeight = configuration.screenHeightDp.dp
+    val density = LocalDensity.current
+    val configuration = LocalConfiguration.current
 
-  // Get system bar and keyboard (IME) insets
-  val systemBarsPadding = WindowInsets.systemBars.asPaddingValues()
-  val imePadding = WindowInsets.ime.asPaddingValues()
+    // Get screen dimensions
+    val screenWidth = configuration.screenWidthDp.dp
+    val screenHeight = configuration.screenHeightDp.dp
 
-  val topPadding = systemBarsPadding.calculateTopPadding()
-  val bottomPadding = maxOf(systemBarsPadding.calculateBottomPadding(), imePadding.calculateBottomPadding())
+    // Get system bar and keyboard (IME) insets
+    val systemBarsPadding = WindowInsets.systemBars.asPaddingValues()
+    val imePadding = WindowInsets.ime.asPaddingValues()
 
-  // Assume a standard TopAppBar height to prevent dragging over it.
-  val topAppBarHeight = 64.dp
+    val topPadding = systemBarsPadding.calculateTopPadding()
+    val bottomPadding =
+        maxOf(systemBarsPadding.calculateBottomPadding(), imePadding.calculateBottomPadding())
 
-  // The actual content area for the screen, minus the app bar.
-  val contentHeight = screenHeight - topPadding - bottomPadding - topAppBarHeight
+    // Assume a standard TopAppBar height to prevent dragging over it.
+    val topAppBarHeight = 64.dp
 
-  // Size and padding of the FAB - matched to design spec
-  val fabSize = 64.dp // Increased size to match design
-  val fabPadding = 16.dp
+    // The actual content area for the screen, minus the app bar.
+    val contentHeight = screenHeight - topPadding - bottomPadding - topAppBarHeight
 
-  // --- Corner Definitions ---
-  val rightBoundary = 0f
-  val leftBoundary = with(density) { -(screenWidth - fabSize - (2 * fabPadding)).toPx() }
-  val bottomBoundary = 0f
-  val topBoundary = with(density) { -(contentHeight - fabSize - (2 * fabPadding)).toPx() }
+    // Size and padding of the FAB - matched to design spec
+    val fabSize = 64.dp // Increased size to match design
+    val fabPadding = 16.dp
 
-  val corners = listOf(
-    Offset(leftBoundary, topBoundary),    // Top-left
-    Offset(rightBoundary, topBoundary),   // Top-right
-    Offset(leftBoundary, bottomBoundary), // Bottom-left
-    Offset(rightBoundary, bottomBoundary), // Bottom-right
-  )
+    // --- Corner Definitions ---
+    val rightBoundary = 0f
+    val leftBoundary = with(density) { -(screenWidth - fabSize - (2 * fabPadding)).toPx() }
+    val bottomBoundary = 0f
+    val topBoundary = with(density) { -(contentHeight - fabSize - (2 * fabPadding)).toPx() }
 
-  // This effect runs whenever the boundaries change (e.g., keyboard appears).
-  LaunchedEffect(leftBoundary, topBoundary, rightBoundary, bottomBoundary) {
-    position.updateBounds(
-      lowerBound = Offset(leftBoundary, topBoundary),
-      upperBound = Offset(rightBoundary, bottomBoundary),
+    val corners = listOf(
+        Offset(leftBoundary, topBoundary),    // Top-left
+        Offset(rightBoundary, topBoundary),   // Top-right
+        Offset(leftBoundary, bottomBoundary), // Bottom-left
+        Offset(rightBoundary, bottomBoundary), // Bottom-right
     )
-  }
 
-
-  FloatingActionButton(
-    onClick = onClick,
-    shape = CircleShape,
-    containerColor = MeTheme.colorScheme.primaryAction, // Solid blue color to match design
-    modifier = Modifier
-      .size(fabSize) // Explicit size to match design
-      .offset { IntOffset(position.value.x.toInt(), position.value.y.toInt()) }
-      .zIndex(1f)
-      .pointerInput(leftBoundary, topBoundary, rightBoundary, bottomBoundary) {
-        detectDragGestures(
-          onDrag = { change, dragAmount ->
-            scope.launch {
-              val newPosition = position.value + dragAmount
-              val coercedPosition = Offset(
-                x = newPosition.x.coerceIn(leftBoundary, rightBoundary),
-                y = newPosition.y.coerceIn(topBoundary, bottomBoundary),
-              )
-              position.snapTo(coercedPosition)
-            }
-          },
-          onDragEnd = {
-            scope.launch {
-              val nearestCorner = corners.minByOrNull { corner ->
-                (corner - position.value).getDistance()
-              }
-              if (nearestCorner != null) {
-                position.animateTo(nearestCorner)
-              }
-            }
-          },
+    // This effect runs whenever the boundaries change (e.g., keyboard appears).
+    LaunchedEffect(leftBoundary, topBoundary, rightBoundary, bottomBoundary) {
+        position.updateBounds(
+            lowerBound = Offset(leftBoundary, topBoundary),
+            upperBound = Offset(rightBoundary, bottomBoundary),
         )
-      },
-  ) {
-    Icon(
-      painter = painterResource(id = AppIcons.Default.WeightOnlyMode),
-      contentDescription = if (showWeightOnlyModeAlert) "Enable Body Metrics" else "Weight Only Mode",
-      modifier = Modifier
-        .size(32.dp)
-        .background(color = Color.Transparent), // Larger icon size to match design
-    )
-  }
+    }
+
+    val elevation: Dp = if (isPressed || isDraggingOn) 8.dp else 0.dp
+    val interactionSource = remember { MutableInteractionSource() }
+    LaunchedEffect(interactionSource) {
+        interactionSource.interactions.collect { interaction ->
+            when (interaction) {
+                is PressInteraction.Press -> isPressed = true
+                is PressInteraction.Release -> isPressed = false
+                is PressInteraction.Cancel -> isPressed = false
+            }
+        }
+    }
+    FloatingActionButton(
+        onClick =
+            onClick,
+        interactionSource = interactionSource,
+        shape = CircleShape,
+        containerColor = if (isPressed || isDraggingOn) MeTheme.colorScheme.primaryAction else MeTheme.colorScheme.primaryAction.copy(
+            alpha = 0.7f
+        ),
+        elevation = FloatingActionButtonDefaults.elevation(elevation),
+        modifier = Modifier
+            .size(fabSize) // Explicit size to match design
+            .offset { IntOffset(position.value.x.toInt(), position.value.y.toInt()) }
+            .clip(CircleShape)
+            .zIndex(1f)
+            .pointerInput(leftBoundary, topBoundary, rightBoundary, bottomBoundary) {
+                detectDragGestures(
+                    onDragStart = { isDraggingOn = true },
+                    onDrag = { change, dragAmount ->
+                        isDraggingOn = true
+                        scope.launch {
+                            val newPosition = position.value + dragAmount
+                            val coercedPosition = Offset(
+                                x = newPosition.x.coerceIn(leftBoundary, rightBoundary),
+                                y = newPosition.y.coerceIn(topBoundary, bottomBoundary),
+                            )
+                            position.snapTo(coercedPosition)
+                        }
+                    },
+                    onDragCancel = { isDraggingOn = false },
+                    onDragEnd = {
+                        isDraggingOn = false
+                        scope.launch {
+                            val nearestCorner = corners.minByOrNull { corner ->
+                                (corner - position.value).getDistance()
+                            }
+                            if (nearestCorner != null) {
+                                position.animateTo(nearestCorner)
+                            }
+                        }
+                    },
+                )
+            },
+    ) {
+        Icon(
+            painter = painterResource(id = AppIcons.Default.WeightOnlyMode),
+            contentDescription = if (showWeightOnlyModeAlert) "Enable Body Metrics" else "Weight Only Mode",
+            modifier = Modifier
+                .size(32.dp)
+                .background(color = Color.Transparent),
+            tint = MeTheme.colorScheme.inverseAction,
+            // Larger icon size to match design
+        )
+    }
 }
 
--- Android/app/src/main/java/com/dmdbrands/gurus/weight/features/common/components/WeightOnlyModePopup.kt
+++ Android/app/src/main/java/com/dmdbrands/gurus/weight/features/common/components/WeightOnlyModePopup.kt
@@ -17,58 +17,57 @@ import com.dmdbrands.gurus.weight.theme.MeTheme.spacing
 
 @Composable
 fun WeightOnlyModePopup(
-  modifier: Modifier = Modifier,
-  onEnable: () -> Unit,
-  onDismiss: () -> Unit
+    modifier: Modifier = Modifier,
+    onEnable: () -> Unit,
+    onDismiss: () -> Unit
 ) {
 
-  Column(
-    modifier = modifier
-      .fillMaxWidth()
-      .padding(bottom = spacing.x3l),
-    verticalArrangement = Arrangement.spacedBy(spacing.md),
-    horizontalAlignment = Alignment.CenterHorizontally,
-  ) {
+    Column(
+        modifier = modifier
+          .fillMaxWidth()
+          .padding(spacing.x2l, end = spacing.x2l, bottom = spacing.x3l),
+        verticalArrangement = Arrangement.spacedBy(spacing.md),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        AppIcon(
+            id = AppIcons.Default.WeightOnlyMode,
+            contentDescription = "Weightonlymode",
+            modifier = Modifier.size(70.dp),
+            type = AppIconType.Primary,
+        )
 
-    AppIcon(
-      id = AppIcons.Default.WeightOnlyMode,
-      contentDescription = "Weightonlymode",
-      modifier = Modifier.size(60.dp),
-      type = AppIconType.Primary,
-    )
+        AppText(
+            text = AppPopupStrings.WeightOnlyModeEnabledPopup.Title,
+            textType = TextType.ListTitle2,
+            textAlign = TextAlign.Center,
+        )
+        AppText(
+            text = AppPopupStrings.WeightOnlyModeEnabledPopup.Message,
+            textType = TextType.Body,
+            textAlign = TextAlign.Center,
+        )
 
-    AppText(
-      text = AppPopupStrings.WeightOnlyModeEnabledPopup.Title,
-      textType = TextType.ListTitle2,
-      textAlign = TextAlign.Center,
-    )
-    AppText(
-      text = AppPopupStrings.WeightOnlyModeEnabledPopup.Message,
-      textType = TextType.Body,
-      textAlign = TextAlign.Center,
-    )
+        AppButton(
+            label = AppPopupStrings.WeightOnlyModeEnabledPopup.ConfirmButton,
+            type = ButtonType.PrimaryFilled,
+            onClick = onEnable,
+        )
 
-    AppButton(
-      label = AppPopupStrings.WeightOnlyModeEnabledPopup.ConfirmButton,
-      type = ButtonType.PrimaryFilled,
-      onClick = onEnable,
-    )
-
-    AppButton(
-      label = AppPopupStrings.WeightOnlyModeEnabledPopup.CancelButton,
-      type = ButtonType.TextPrimary,
-      onClick = onDismiss,
-    )
-  }
+        AppButton(
+            label = AppPopupStrings.WeightOnlyModeEnabledPopup.CancelButton,
+            type = ButtonType.TextPrimary,
+            onClick = onDismiss,
+        )
+    }
 }
 
 @PreviewTheme
 @Composable
 fun WeightOnlyModeEnabledPopupPreview() {
-  MeAppTheme {
-    WeightOnlyModePopup(
-      onEnable = {},
-      onDismiss = {},
-    )
-  }
+    MeAppTheme {
+        WeightOnlyModePopup(
+            onEnable = {},
+            onDismiss = {},
+        )
+    }
 }
