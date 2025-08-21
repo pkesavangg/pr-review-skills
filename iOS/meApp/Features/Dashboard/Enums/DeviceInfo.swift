62c0bd327bd83644886e7e0a55e95be52ab02af1
LakshmiPriyaAnbu <alakshmipriya@greatergoods.com>
2025-08-21 18:42:27 +0530

    Refactor DevicePlatform to use userInterfaceIdiom for iPad detection; update StreakCardView to adjust icon size based on device type.

--- iOS/meApp/Features/Dashboard/Enums/DeviceInfo.swift
+++ iOS/meApp/Features/Dashboard/Enums/DeviceInfo.swift
@@ -6,6 +6,12 @@ enum DevicePlatform {
     /// Returns true if the real device model is an iPad.
     /// Uses `UIDevice.current.model` which reports "iPad" on iPad hardware.
     static var isTablet: Bool {
-        UIDevice.current.model.range(of: "iPad", options: .caseInsensitive) != nil
+        UIDevice.current.userInterfaceIdiom == .pad
     }
-} 
+    static var isSmallPhone: Bool {
+        !isTablet && min(UIScreen.main.nativeBounds.width, UIScreen.main.nativeBounds.height) < 1200
+    }
+    static var isLargePhone: Bool {
+        !isTablet && !isSmallPhone
+    }
+}
