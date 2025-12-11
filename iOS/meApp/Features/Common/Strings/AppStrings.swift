8f03bf9a1c982fa3e853e82ef6dc0902d5254191
LakshmiPriyaAnbu <alakshmipriya@greatergoods.com>
2025-12-11 12:38:08 +0530

    Refactor Login and Signup forms to enhance email validation messages and update max length constraints for better user feedback.

--- iOS/meApp/Features/Common/Strings/AppStrings.swift
+++ iOS/meApp/Features/Common/Strings/AppStrings.swift
@@ -249,6 +249,7 @@ struct FormErrorMessages {
     static let required = "must not be left blank"
     static let leaveBlank = "must not leave blank"
     static let email = "must use a valid email"
+    static let emailMaxLength = "Email should not exceed 100 characters"
     static let passwordMaxLength = "password should not exceed 50 characters"
     static let passwordMinLength = "password must be 6 characters long"
     static let minLength = { (length: Int) in "minimum value should be \(length)" }
