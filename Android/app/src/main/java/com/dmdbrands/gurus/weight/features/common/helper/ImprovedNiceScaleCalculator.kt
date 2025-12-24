b2fc0ed0e06daa8b54e414bb899f7dd238790348
rengadevi <vrengadevi@greatergoods.com>
2025-12-24 11:19:03 +0530

    Refactor: Align Y-axis tick calculation with iOS
    Updated the `niceNumbers` list in the `ImprovedNiceScaleCalculator` to include `2.0` and `200.0`. This change aligns the Android graph's Y-axis tick calculation with the implementation used in the iOS version, ensuring a consistent user experience across platforms.

--- Android/app/src/main/java/com/dmdbrands/gurus/weight/features/common/helper/ImprovedNiceScaleCalculator.kt
+++ Android/app/src/main/java/com/dmdbrands/gurus/weight/features/common/helper/ImprovedNiceScaleCalculator.kt
@@ -11,8 +11,8 @@ data class AxisMeta(
 
 object ImprovedNiceScaleCalculator {
   // Improved algorithm for Y-axis tick calculation optimized for gradual weight changes
-
-  private val niceNumbers = listOf(1.0, 4.0, 5.0, 10.0, 15.0, 20.0, 25.0, 40.0, 50.0, 100.0)
+  // To follow the same IOS followed
+  private val niceNumbers = listOf(1.0, 2.0, 4.0, 5.0, 10.0, 15.0, 20.0, 25.0, 40.0, 50.0, 100.0, 200.0)
 
   fun generateNiceScale(
     minValue: Double,
