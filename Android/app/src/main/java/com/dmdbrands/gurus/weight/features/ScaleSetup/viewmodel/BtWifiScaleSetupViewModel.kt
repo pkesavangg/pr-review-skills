8bbee42f587d195f3e5236aaa20f7339d45a0008
kaviyagg <mkaviya@greatergoods.com>
2026-02-11 15:27:51 +0530

    MA-2754 Implement stopObservingDevices call to prevent dialog stacking when a known scale is discovered

--- Android/app/src/main/java/com/dmdbrands/gurus/weight/features/ScaleSetup/viewmodel/BtWifiScaleSetupViewModel.kt
+++ Android/app/src/main/java/com/dmdbrands/gurus/weight/features/ScaleSetup/viewmodel/BtWifiScaleSetupViewModel.kt
@@ -1950,9 +1950,10 @@ constructor(
                 ggDeviceDetail.broadcastIdString,
               )
             ) {
-              // Cancel timeout since we found a known scale
+              // Cancel timeout and stop scan to prevent dialog stacking from repeated callbacks
               pairingTimeoutJob?.cancel()
               pairingTimeoutJob = null
+              stopObservingDevices()
 
               dialogQueueService.showDialog(
                 DialogModel.Alert(
