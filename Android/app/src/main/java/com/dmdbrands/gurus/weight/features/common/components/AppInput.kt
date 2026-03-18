ac561b61dfdafb847d993e91cfddd624dd23a400
kaviyagg <mkaviya@greatergoods.com>
2026-03-18 09:31:29 +0530

    MA-3361 Remove unused import of rememberCoroutineScope in AppInput.kt to streamline code and enhance readability.

--- Android/app/src/main/java/com/dmdbrands/gurus/weight/features/common/components/AppInput.kt
+++ Android/app/src/main/java/com/dmdbrands/gurus/weight/features/common/components/AppInput.kt
@@ -22,7 +22,6 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
